a57f0a9950241c02b9a56f4f680e17553050af5a
Divya <divyajyothi139@gmail.com>
2026-01-30 04:15:28 +0800

    added nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
+import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main data-theme="cupcake" className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to My Next.js App</h1>
-     <Link href="/users">Go to Users Page</Link>
-     <div>
-      <ProductCard/>
-     </div>
-     
+    <main
+      data-theme="cupcake"
+    >
+      <div className="flex min-h-screen flex-col items-center justify-between" >
+        <NavBar /> 
+
+        {/* <Link href="/users">Go to Users Page</Link> */}
+        <div className="pt-4">
+          <ProductCard />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
