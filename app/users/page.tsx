55051a6f67186dfd5e07dc7c147a6c63aefcf4b9
Divya <divyajyothi139@gmail.com>
2026-01-30 03:27:08 +0800

    first commit with initial home page

---
+++ app/users/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users: User[] = await res.json();
+    console.log(users);
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} ({user.username}) - {user.email}
+          </li>
+        ))}             
+      </ul>
+    </>
+  )
+}
+
+export default UsersPage
