6c65829e5474a94922537d5217062ab9175abf8f
Divya <divyajyothi139@gmail.com>
2026-02-04 23:57:57 +0800

    Fix Prisma client path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,14 @@
 import "dotenv/config";
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-export { prisma }
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
